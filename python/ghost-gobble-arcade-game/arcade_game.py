d420ce6b22c961071f2a7cf95165fe1b9dba1111
Grafton Cook <grafton.cook@gmail.com>
2023-04-16 08:17:55 -0400

    Updated README.md and finished arcade_game.py

--- python/ghost-gobble-arcade-game/arcade_game.py
+++ python/ghost-gobble-arcade-game/arcade_game.py
@@ -9,7 +9,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :return: bool - can the ghost be eaten?
     """
 
-    pass
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -20,18 +20,18 @@ def score(touching_power_pellet, touching_dot):
     :return: bool - has the player scored or not?
     """
 
-    pass
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
-    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his power pellet.
+    """Trigger the game loop to end when Pac-Man touches a ghost without his power pellet.
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player lost the game?
     """
 
-    pass
+    return touching_ghost and not power_pellet_active
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -43,4 +43,4 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    pass
+    return has_eaten_all_dots and ((power_pellet_active and touching_ghost) or (not power_pellet_active and not touching_ghost))

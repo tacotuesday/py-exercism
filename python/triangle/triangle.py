9875c07dc794d2a71ea93b68c84858bc189998e2
Grafton Cook <grafton.cook@gmail.com>
2023-04-16 08:34:12 -0400

    Edited Triangle README.md and clarified arg type for each function.

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,22 @@
-def equilateral(sides):
+def equilateral(sides: list) -> bool:
+    """
+    Determines whether the triangle is equilateral. An equilateral triangle has three sides of equal length.
+    :param sides:
+    """
     pass
 
 
-def isosceles(sides):
+def isosceles(sides: list) -> bool:
+    """
+    Determines whether the triangle is isosceles. An isosceles triangle has at least two sides the same length.
+    :param sides:
+    """
     pass
 
 
-def scalene(sides):
+def scalene(sides: list) -> bool:
+    """
+    Determines whether the triangle is scalene. Scalene triangles have no sides of equal length.
+    :param sides:
+    """
     pass

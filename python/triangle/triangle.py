fb70fa859aa436281fe8bc092dbb77b604b297a2
Grafton Cook <grafton.cook@gmail.com>
2023-04-16 08:44:36 -0400

    First draft of triangle.py.

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -3,7 +3,8 @@ def equilateral(sides: list) -> bool:
     Determines whether the triangle is equilateral. An equilateral triangle has three sides of equal length.
     :param sides:
     """
-    pass
+    check_side_length_uniqueness = set(list)
+    return len(check_side_length_uniqueness) == 1
 
 
 def isosceles(sides: list) -> bool:
@@ -11,7 +12,8 @@ def isosceles(sides: list) -> bool:
     Determines whether the triangle is isosceles. An isosceles triangle has at least two sides the same length.
     :param sides:
     """
-    pass
+    check_side_length_uniqueness = set(list)
+    return len(check_side_length_uniqueness) == 2
 
 
 def scalene(sides: list) -> bool:
@@ -19,4 +21,5 @@ def scalene(sides: list) -> bool:
     Determines whether the triangle is scalene. Scalene triangles have no sides of equal length.
     :param sides:
     """
-    pass
+    check_side_length_uniqueness = set(list)
+    return len(check_side_length_uniqueness) == 3

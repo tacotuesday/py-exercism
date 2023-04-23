045c1eb19973e1c71bca394a128c4d39b3641dd2
Grafton Cook <grafton.cook@gmail.com>
2023-04-23 09:14:07 -0400

    Removed  casting from function returns

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -15,7 +15,7 @@ def equilateral(sides: list) -> bool:
     Determines whether the triangle is equilateral. An equilateral triangle has three sides of equal length.
     :param sides:
     """
-    return bool(is_triangle(sides) and len(set(sides)) == 1)
+    return is_triangle(sides) and len(set(sides)) == 1
 
 
 def isosceles(sides: list) -> bool:
@@ -23,7 +23,7 @@ def isosceles(sides: list) -> bool:
     Determines whether the triangle is isosceles. An isosceles triangle has at least two sides the same length.
     :param sides:
     """
-    return bool(is_triangle(sides) and len(set(sides)) <= 2)
+    return is_triangle(sides) and len(set(sides)) <= 2
 
 
 def scalene(sides: list) -> bool:
@@ -31,4 +31,4 @@ def scalene(sides: list) -> bool:
     Determines whether the triangle is scalene. Scalene triangles have no sides of equal length.
     :param sides:
     """
-    return bool(is_triangle(sides) and len(set(sides)) == 3)
+    return is_triangle(sides) and len(set(sides)) == 3

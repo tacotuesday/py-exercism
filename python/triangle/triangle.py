551df1f97699bbfcf3b755ac3bf752fec0a446ac
Grafton Cook <grafton.cook@gmail.com>
2023-04-23 10:38:57 -0400

    Created an @triangle decorator to enforce triangle validity

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,34 +1,37 @@
 """ Group of functions to classify three-member lists as different types of triangles. This would make a great set
 of class methods, but that probably wouldn't pass tests."""
 
+def triangle(func):
+    def is_triangle(sides):
+        if sum(sides) != 0 and (
+            sides[0] + sides[1] >= sides[2]
+            and sides[1] + sides[2] >= sides[0]
+            and sides[0] + sides[2] >= sides[1]
+        ): return func(sides)
+        else:
+            return False
+    return is_triangle
 
-def is_triangle(sides):
-    return sum(sides) != 0 and (
-        sides[0] + sides[1] >= sides[2]
-        and sides[1] + sides[2] >= sides[0]
-        and sides[0] + sides[2] >= sides[1]
-    )
-
-
+@triangle
 def equilateral(sides: list) -> bool:
     """
     Determines whether the triangle is equilateral. An equilateral triangle has three sides of equal length.
     :param sides:
     """
-    return is_triangle(sides) and len(set(sides)) == 1
+    return len(set(sides)) == 1
 
-
+@triangle
 def isosceles(sides: list) -> bool:
     """
     Determines whether the triangle is isosceles. An isosceles triangle has at least two sides the same length.
     :param sides:
     """
-    return is_triangle(sides) and len(set(sides)) <= 2
+    return len(set(sides)) <= 2
 
-
+@triangle
 def scalene(sides: list) -> bool:
     """
     Determines whether the triangle is scalene. Scalene triangles have no sides of equal length.
     :param sides:
     """
-    return is_triangle(sides) and len(set(sides)) == 3
+    return len(set(sides)) == 3

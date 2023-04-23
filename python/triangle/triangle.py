3587e912e2d8512c40d39552d1323631291a8e17
Grafton Cook <grafton.cook@gmail.com>
2023-04-23 10:49:53 -0400

    Added docstring content

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,7 +1,12 @@
 """ Group of functions to classify three-member lists as different types of triangles. This would make a great set
 of class methods, but that probably wouldn't pass tests."""
 
-def triangle(func):
+def triangle(func: callable) -> callable:
+    """Decorator to check if the sides of a triangle are valid. If not, return False.
+
+    :param func: Function to be decorated
+    :type func: function
+    """       
     def is_triangle(sides):
         if sum(sides) != 0 and (
             sides[0] + sides[1] >= sides[2]
@@ -16,7 +21,7 @@ def triangle(func):
 def equilateral(sides: list) -> bool:
     """
     Determines whether the triangle is equilateral. An equilateral triangle has three sides of equal length.
-    :param sides:
+    :param sides: List of three sides of a triangle
     """
     return len(set(sides)) == 1
 
@@ -24,7 +29,7 @@ def equilateral(sides: list) -> bool:
 def isosceles(sides: list) -> bool:
     """
     Determines whether the triangle is isosceles. An isosceles triangle has at least two sides the same length.
-    :param sides:
+    :param sides: List of three sides of a triangle
     """
     return len(set(sides)) <= 2
 
@@ -32,6 +37,6 @@ def isosceles(sides: list) -> bool:
 def scalene(sides: list) -> bool:
     """
     Determines whether the triangle is scalene. Scalene triangles have no sides of equal length.
-    :param sides:
+    :param sides: List of three sides of a triangle
     """
     return len(set(sides)) == 3
